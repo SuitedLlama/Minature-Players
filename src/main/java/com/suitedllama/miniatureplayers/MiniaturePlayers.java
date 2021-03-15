a6951faba9ebe0ce57fb3acd7f73ddf88f505485
suitedllama <lukewines02+github@gmail.com>
2021-03-15 09:29:22 -0700

    setup

---
+++ src/main/java/com/suitedllama/miniatureplayers/MiniaturePlayers.java
@@ -0,0 +1,11 @@
+package com.suitedllama.miniatureplayers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MiniaturePlayers implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+		System.out.println("Hello Fabric world!");
+	}
+}
--- src/main/java/com/suitedllama/miniatureplayers/mixin/ExampleMixin.java
+++ src/main/java/com/suitedllama/miniatureplayers/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package com.suitedllama.miniatureplayers.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
